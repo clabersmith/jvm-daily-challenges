a7a1ff686c36500ad1d0cb743acd4b2ecf690df2
clabersmith <clabersmith@gmail.com>
2026-01-08 17:59:10 -0800

    solutions for day 12

--- app/src/main/java/Day12.java
+++ app/src/main/java/Day12.java
@@ -1,8 +1,29 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 public class Day12 {
     public static BigDecimal findAverage(List<Integer> list) {
-        return null;
+        return usingLoops(list);
+    }
+
+    private static BigDecimal usingLoops(List<Integer> list) {
+        BigDecimal sum = BigDecimal.ZERO;
+
+        for(int val : list) {
+            sum = sum.add(BigDecimal.valueOf(val));
+        }
+
+        return sum.divide(BigDecimal.valueOf(list.size()), 15,
+                RoundingMode.HALF_UP);
+    }
+
+    private static BigDecimal usingStreams(List<Integer> list) {
+        BigDecimal sum = list.stream()
+                .map(BigDecimal::valueOf)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        return sum.divide(BigDecimal.valueOf(list.size()), 15,
+                RoundingMode.HALF_UP);
     }
 }
--- app/src/main/kotlin/Day12.kt
+++ app/src/main/kotlin/Day12.kt
@@ -1,3 +1,8 @@
 import java.math.BigDecimal
 
-fun findAverage(list : List<Int>) : BigDecimal? = null
+fun findAverage(list : List<Int>) : BigDecimal?  {
+    val sum = list.fold(BigDecimal.ZERO) { acc, v -> acc + BigDecimal.valueOf(v.toLong()) }
+
+    return sum.divide(BigDecimal.valueOf(list.size.toLong()), 15,
+        java.math.RoundingMode.HALF_UP)
+}
