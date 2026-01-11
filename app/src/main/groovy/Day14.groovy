c2efc7ea67fad2823b66df35f49238b01c33f8cb
clabersmith <clabersmith@gmail.com>
2026-01-11 15:52:52 -0800

    set up challenge for day 14

---
+++ app/src/main/groovy/Day14.groovy
@@ -0,0 +1,5 @@
+class Day14Groovy {
+    static <E> List<E> mergeSortedLists(List<E> list1, List<E> list2) {
+        [] as List<E>
+    }
+}
---
+++ app/src/main/kotlin/Day14.kt
@@ -0,0 +1,3 @@
+fun <E : Comparable<E>> mergeSortedLists(list1: List<E>, list2: List<E>) : List<E> {
+    return emptyList()
+}
