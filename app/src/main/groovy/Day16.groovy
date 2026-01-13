23b701bf5c9a801b328f5fdcc463a810e7b6e645
clabersmith <clabersmith@gmail.com>
2026-01-13 12:39:42 -0800

    solutions for day 16

--- app/src/main/groovy/Day16.groovy
+++ app/src/main/groovy/Day16.groovy
@@ -1,5 +1,6 @@
 class Day16Groovy {
     static <E extends Comparable> List<E> findIntersection(List<E> list1, List<E> list2) {
-        [] as List<E>
+        if (list1 == null || list2 == null) return [] as List<E>
+        return list1.intersect(list2).unique().sort()
     }
 }
