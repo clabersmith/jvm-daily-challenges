b9ea0cdcc2ace3347438c5f2de96a3380883ea22
clabersmith <clabersmith@gmail.com>
2026-01-12 20:51:46 -0800

    solutions for day 15

--- app/src/main/java/Day15.java
+++ app/src/main/java/Day15.java
@@ -1,7 +1,40 @@
 import java.util.List;
+import java.util.Objects;
 
 public class Day15 {
     public static <E extends Comparable<E>> E findSecondLargest(List<E> list) {
-        return null;
+        if(list == null || list.contains(null) || list.size() < 2) {
+            return null;
+        }
+
+        return usingLoops(list);
+    }
+
+    private static <E extends Comparable<E>> E usingLoops(List<E> list) {
+        E largest = null;
+        E second = null;
+
+        for (E item : list) {
+            if (largest == null || item.compareTo(largest) > 0) {
+                second = largest;
+                largest = item;
+            } else if (item.compareTo(largest) < 0) {
+                if (second == null || item.compareTo(second) > 0) {
+                    second = item;
+                }
+            }
+        }
+
+        return second; //returns as null if list contains only duplicates
+    }
+
+    private static <E extends Comparable<E>> E usingStreams(List<E> list) {
+        return list.stream()
+                .filter(Objects::nonNull)
+                .distinct()
+                .sorted(java.util.Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .orElse(null);
     }
 }
