e1f6b08ff98f0476953d2dec9829577543718395
clabersmith <clabersmith@gmail.com>
2026-01-19 19:31:56 -0800

    set up challenge for day 21

---
+++ app/src/main/groovy/Day21.groovy
@@ -0,0 +1,5 @@
+class Day21Groovy {
+    static <T extends Comparable> int binarySearchSortedList(List<T> list, T item) {
+        -1
+    }
+}
---
+++ app/src/main/java/Day21.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Day21 {
+
+    public static <E extends Comparable<E>> int binarySearchSortedList(List<E> list, E item) {
+        return -1;
+    }
+
+}
